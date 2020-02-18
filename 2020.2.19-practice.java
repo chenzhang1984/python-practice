ac556a1e64b7cb6da95bc86fb3ebea440b42bbb6
chenzhang1984 <36017189@qq.com>
2020-02-19 00:34:16 +0800

    Update and rename 2020.2.19-practice.cs to 2020.2.19-practice.java

--- 2020.2.19-practice.java
+++ 2020.2.19-practice.java
@@ -1,11 +1,8 @@
-public class Solution
-{
-    public int RemoveDuplicates(int[] nums)
-    {
-        if (nums.Length == 0)
-            return 0;
+//2020.2.19 算法题：删除排序数组中的重复项（java代码）
+class Solution {
+    public int removeDuplicates(int[] nums) {
         int j = 1;
-        for (int i = 1; i < nums.Length; i++)
+        for (int i = 1; i < nums.length; i++)
         {
             if (nums[i] > nums[j - 1])
             {
